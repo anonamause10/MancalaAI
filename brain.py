d1cb3b6188fd5f126355ec37d6181ee2154419bc
anonamause10 <anonamause10@gmail.com>
2020-07-29 15:05:42 -0500

    added saving system

--- brain.py
+++ brain.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 class Brain:
     
@@ -7,19 +8,18 @@ class Brain:
         self.biases = []
         self.inputshape = numinputs
         self.outputshape = numoutputs
+        self.currepoch = 0
         r = numinputs
         for i in range(len(hiddenlayersizes)+1):
             if(i<len(hiddenlayersizes)):
                 c = hiddenlayersizes[i]
             else:
                 c = numoutputs
-            weightmatrix = np.ones((r,c))
+            weightmatrix = np.ones((c,r))
             biasvector = np.zeros((c))
             self.weights.append(weightmatrix)
             self.biases.append(biasvector)
             r = c
-        print(self.weights[0])
-        print(self.biases[0])
 
     def evaluate(self,input):
         output = np.copy(input)
@@ -34,17 +34,52 @@ class Brain:
         return output
 
     def actionindex(self,outputvec):
-        return np.argmax(outputvec)[0]
+        return np.argmax(outputvec)
 
-    def mutate(self,weightfactor,biasfactor):
+    def mutate(self,weightfactor = 1,biasfactor = 1):
         
         for i in range(len(self.weights)):
             w =  self.weights[i]
-            self.weights[i] = np.add(w,np.random.rand(w.shape[0],w.shape[1])*weightfactor)
+            self.weights[i] = np.add(w,(np.random.rand(w.shape[0],w.shape[1])-0.5)*2*weightfactor)
             b = self.biases[i]
-            self.biases[i] = np.add(b,np.random.rand(b.shape[0])*biasfactor) 
+            self.biases[i] = np.add(b,(np.random.rand(b.shape[0])-0.5)*2*biasfactor) 
+        self.currepoch += 1
+
+    def setlayers(self, newbrain):
+        self.weights = np.copy(newbrain.weights)
+        self.biases = np.copy(newbrain.biases)
+        self.inputshape = newbrain.inputshape
+        self.outputshape = newbrain.outputshape
+        self.currepoch = newbrain.currepoch
+
+    def printweights(self):
+        print("Weights:")
+        for i in range(len(self.weights)):
+            print("Layer " + str(i) + " weights:")
+            print(self.weights[i])
+
+    def printbiases(self):
+        print("Biases:")
+        for i in range(len(self.biases)):
+            print("Layer " + str(i) + " biases:")
+            print(self.biases[i])
+
+    def save(self):
+        for i in range(len(self.weights)):
+            np.save("weights/weights" + str(i),self.weights[i])
+            np.save("biases/biases" + str(i),self.biases[i])
+
+    def load(self):
+        self.weights = []
+        self.biases = []
+        for f in os.listdir("weights"):
+            self.weights.append(np.load(f))
 
 
             
         
 brain = Brain([10,10,10],14,6)
+brain.mutate()
+brain.printweights()
+print(brain.actionindex(brain.evaluate(np.array([4,4,4,4,4,4,0,4,4,4,4,4,4,0]))))
+brain.save()
